ce9ba5111d73eb81db0ac39cd455e83fdfaff234
VarunTQQber <122548038+VarunTQQber@users.noreply.github.com>
2023-01-26 10:22:24 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    counterVar += 1
+    basic.showString("" + (counterVar))
+})
+input.onButtonPressed(Button.AB, function () {
+    counterVar = 0
+    basic.showString("" + (counterVar))
+})
+input.onButtonPressed(Button.B, function () {
+    counterVar += -1
+    basic.showString("" + (counterVar))
+})
+let counterVar = 0
+counterVar = 0
+basic.showString("" + (counterVar))
+basic.forever(function () {
+	
+})
