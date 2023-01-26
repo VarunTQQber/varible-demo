f7fbd34d8a7c1a74d55eef2a4dc21cf977a47419
VarunTQQber <122548038+VarunTQQber@users.noreply.github.com>
2023-01-26 11:10:26 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     counterVar += 1
-    basic.showNumber(counterVar)
+    basic.showNumber(counterVar,50)
 })
 input.onButtonPressed(Button.AB, function () {
     counterVar = 0
-    basic.showNumber(counterVar)
+    basic.showNumber(counterVar,50)
 })
 input.onButtonPressed(Button.B, function () {
     counterVar += -1
-    basic.showNumber(counterVar)
+    basic.showNumber(counterVar,50)
 })
 let counterVar = 0
 counterVar = 0
-basic.showNumber(counterVar)
-basic.forever(function () {
-	
-})
+basic.showNumber(counterVar,50)
